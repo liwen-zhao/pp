dd27e4f03a109da9d212c694f72c64a606a2a564
赵丽文 <66107396+liwen-zhao@users.noreply.github.com>
2024-07-11 15:10:22 +0800

    Update networks.py

--- networks.py
+++ networks.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
-from .attention import cbamAttention,SEAttention,GAM_Attention,simam_module
+#from .attention import cbamAttention,SEAttention,GAM_Attention,simam_module
 
 ###############################################################################
 # Helper Functions
